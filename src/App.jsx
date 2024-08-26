113be4020328428ce2cf85b564ac421544b8ecaa
Ankurdyno <98178905+Ankurdyno@users.noreply.github.com>
2024-08-26 19:10:56 +0530

    chor:remove testimonial api

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./App.css";
 import Navbar from "./components/common/Navbar";
 import AppContext from "./AppContext";
